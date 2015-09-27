3c18a5d8696edff8b6900b0dd70084dd09eef528
dwadelson <dwadelson@gmail.com>
2015-09-26 22:49:19 -0700

    add initWithCoder and initWithFrame (skeleton) for IFLInfoButton

--- DACustomViews/IFLInfoButton.m
+++ DACustomViews/IFLInfoButton.m
@@ -10,13 +10,33 @@
 
 @implementation IFLInfoButton
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+
+-(id) initWithFrame:(CGRect)frame {
+    
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+    }
+    return self;
 }
-*/
+
+
+//initWithCoder is invoked when you have the item instantiated via a storyboard
+- (id) initWithCoder:(NSCoder *)aDecoder {
+    
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        
+        //1. Load Interface View
+        
+        [[NSBundle mainBundle] loadNibNamed:@"IFLInfoButton" owner:self options:nil];
+        
+        //2. Load subview
+        [self addSubview:self.view];
+        
+    }
+    return self;
+}
 
 - (IBAction)buttonPressed:(UIButton *)sender {
 }
